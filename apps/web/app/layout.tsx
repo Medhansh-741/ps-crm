160b0a2b89819d01ddfde8e6b1d7314ec59b4e2c
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-03-04 12:50:15 +0530

    fixed leaflet issue

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";   
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
